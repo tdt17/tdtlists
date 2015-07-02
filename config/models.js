dc09c8108a91e9be06c7ace30b056d3b7547fb91
Torsten Hain <tdt.hain@gmail.com>
2015-07-02 03:58:22 +0200

    added a migration settings

--- config/models.js
+++ config/models.js
@@ -11,22 +11,7 @@
 
 module.exports.models = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  connection: 'mongoDb'
-
-  /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-  // migrate: 'alter'
+  connection: 'mongoDb',
+  migrate: 'alter'
 
 };
